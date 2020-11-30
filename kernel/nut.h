e8571cf81228690dfb7cc0721d4c1ac4f9ea2647
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-11-30 20:35:31 +0000

    Create set_color(fore, back) function

--- kernel/nut.h
+++ kernel/nut.h
@@ -43,6 +43,10 @@ int ASCII_NUMBERS[10] = {
 static u8 actual_back_color = BLACK;
 static u8 actual_fore_color = WHITE;
 
+#define set_color(fore, back)\
+	actual_fore_color = fore;
+	actual_back_color = back
+
 
 #include "utils.h"
 #include "keys.h"
