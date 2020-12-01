1abab682321e7f3d97b1b7e12b32b2951939eac3
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-01 15:34:31 +0000

    Create function halt() & rename abort to panic

--- kernel/abort.h
+++ kernel/abort.h
@@ -2,11 +2,15 @@
 #define __NUT_ABORT_H__
 
 __attribute__((__noreturn__))
-function abort(String msg) {
-	Log(msg, RED, actual_back_color);
-
+function halt() {
 	while (TRUE) {}
 	__builtin_unreachable();
+}
+
+function panic(String msg) {
+	Log(msg, RED, actual_back_color);
+
+	halt();
 }
 
 #endif //nut abort module
