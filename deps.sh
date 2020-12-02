92f2f67aa8dc66ebc2a4ba25c767dcfda174a5d7
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-02 18:05:14 +0000

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,12 +1,5 @@
 # nut deps installer for Linux
-if [ $1 = "apt" ]; then
-	sudo apt update
-	sudo apt install gcc
-	sudo apt install bison
-	sudo apt install flex
-
-elif [ $1 = "pacman" ];
-	pacman -S gcc
-	pacman -S bison
-	pacman -S flex
-fi
+sudo apt update
+sudo apt install gcc
+sudo apt install bison
+sudo apt install flex
