ff79fc0c4fa8f6d337634b93f665869154fcfc3d
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-06 12:22:19 +0000

    Add Image data type and DrawImage function

--- kernel/nut/image.h
+++ kernel/nut/image.h
@@ -1,10 +1,12 @@
 #ifndef __NUT_IMAGE_H__
 #define __NUT_IMAGE_H__
 
-typedef u8 VGA_Map[HEIGHT][WIDTH];
+typedef u8 VGAMap[HEIGHT][WIDTH];
+typedef u8 Image[][WIDTH];
 
 
-function DrawMap(VGA_Map map) {
+function DrawMap(VGAMap map) {
+	clear_screen(BLACK, BLACK);
 	for (int i = 0; i < VGA_HEIGHT; i++) {
 		for (int j = 0; j < VGA_WIDTH; j++) {
 			gotoxy(j, i);
@@ -13,5 +15,16 @@ function DrawMap(VGA_Map map) {
 	}
 }
 //Draw VGA map
+
+function DrawImage(Image im) {
+	clear_screen(BLACK, BLACK);
+	for (int i = 0; i < (sizeof(im) / sizeof(im[0])); i++) {
+		for (int j = 0; j < (sizeof(im[0]) / sizeof(im[0][0])); j++) {
+			gotoxy(j, i);
+			charout(' ', BLACK, (color)im[i][j]);
+		}
+	}
+}
+//Draw image
 
 #endif //nut image render module
