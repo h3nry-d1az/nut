3d4b5eacdca735fa4df3a04939e2f77d6712a43f
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-08 16:41:16 +0000

    Add extern functions to nut.rs

--- nut.rs/src/lib.rs
+++ nut.rs/src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// This file contains the
+// nut kernel wrapper for
+// rust developers.
+//
+// If you want to make an O.S.
+// in rust, you should use the
+// nut kernel with the rust wrapper
+
+#![no_std]
+// Don't use std crate
+
+extern {
 }
+// Please call these functions
+// inside an 'unsafe {}' code block
