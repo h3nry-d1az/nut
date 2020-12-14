2c02b0ec7c09bcf0f9de4482cb70e25728ac66f3
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-14 16:00:35 +0000

    Create cursor module

---
+++ kernel/modules/cursor.h
@@ -0,0 +1,19 @@
+#ifndef __NUT_CURSOR_M__
+#define __NUT_CURSOR_M__
+
+#include <nut/types.h>
+
+function EnableCursor(u8 cursor_start, u8 cursor_end) {
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
+
+	outb(0x3D4, 0x0B);
+	outb(0x3D5, (inb(0x3D5) & 0xE0) | cursor_end);
+}
+
+function DisableCursor() {
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, 0x20);
+}
+
+#endif //nut cursor module
