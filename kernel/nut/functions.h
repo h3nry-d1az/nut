d9a2315cf567361f3fa692ab6caa8b2a02bab29c
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-17 20:30:20 +0000

    Update kernel functions module

--- kernel/nut/functions.h
+++ kernel/nut/functions.h
@@ -1,49 +1,21 @@
 #ifndef __NUT_FUNCTIONS_H__
 #define __NUT_FUNCTIONS_H__
 
-u16 vga_entry(unsigned char ch, u8 fore_color, u8 back_color);
-function clear_vga_buffer(u16** buffer, u8 fore_color, u8 back_color);
-function init_vga(u8 fore_color, u8 back_color);
-//VGA functions
+function halt();
+function panic(String msg);
+//abort functions
 
-u32 strlen(const String str);
-u32 digits(int num);
-function ItoA(int num, String number);
-//util functions
+FAT32_Sector InitFAT32(u32 size, u16 fversion,
+	u32 rcluster, u16 info,
+	u16 backup, u8 number,
+	u32 id);
+//files functions
 
-u8 inb(u16 port);
-function outb(u16 port, u8 data);
-char charin();
-String strin();
-function newline();
-function charout(char character, u8 fore_color, u8 back_color);
-function strout(String str, u8 fore_color, u8 back_color);
-function centered_strout(String str, u8 fore_color, u8 back_color);
-function intout(int num, u8 fore_color, u8 back_color);
-function gotox(u16 x);
-function gotoy(u16 y);
-function gotoxy(u16 x, u16 y);
-function clear_screen(u8 fore_color, u8 back_color);
-//I/O functions
+function Log(String msg, u8 fore_color, u8 back_color);
+//log functions
 
-u16 GetBoxDrawChar(u8 chn, u8 fore_color, u8 back_color);
-function DrawSimpleBox(u16 x, u16 y,
-    u16 width, u16 height,
-    u8 fore_color, u8 back_color,
-    u8 topleft_ch,
-    u8 topbottom_ch,
-    u8 topright_ch,
-    u8 leftrightside_ch,
-    u8 bottomleft_ch,
-    u8 bottomright_ch);
-function DrawBox(u8 boxtype,
-    u16 x, u16 y,
-    u16 width, u16 height,
-    u8 fore_color, u8 back_color);
-function FillBox(u8 ch,
-    u16 x, u16 y,
-    u16 width, u16 height,
-    u8 color);
-//UI functions
+u16 switch_endian16(u16 nb);
+u32 switch_endian32(u32 nb)
+//network functions
 
 #endif //nut function prototypes
