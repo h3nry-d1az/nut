0ef839572d9b22dae3f2ef7d432b752cd379fb66
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-18 21:22:36 +0000

    Fix some errors

--- kernel/nut/functions.h
+++ kernel/nut/functions.h
@@ -5,17 +5,11 @@ function halt();
 function panic(String msg);
 //abort functions
 
-FAT32_Sector InitFAT32(u32 size, u16 fversion,
-	u32 rcluster, u16 info,
-	u16 backup, u8 number,
-	u32 id);
-//files functions
-
 function Log(String msg, u8 fore_color, u8 back_color);
 //log functions
 
 u16 switch_endian16(u16 nb);
-u32 switch_endian32(u32 nb)
+u32 switch_endian32(u32 nb);
 //network functions
 
 #endif //nut function prototypes
--- kernel/nut/nut.c
+++ kernel/nut/nut.c
@@ -3,14 +3,14 @@ info in wiki.osdev.org */
 #ifndef __NUT_KERNEL__
 #define __NUT_KERNEL__
 
-void NutInit() { return };
+void NutInit() { return; };
 const char* NUT_VERSION = "v0.2.3";
 
 #include <nut/types.h>
 #include <nut/utils.h>
 #include <nut/colors.h>
 #include <nut/basics.h>
-#include <nut/stdio.h>
+#include <nut/io.h>
 
 #include "functions.h"
 #include "log.h"
