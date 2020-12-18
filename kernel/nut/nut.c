8bc74ce92229334a2778f8cc8b16057e5d1e7e23
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-18 18:41:59 +0000

    Add NutInit function to nut main file

--- kernel/nut/nut.c
+++ kernel/nut/nut.c
@@ -3,24 +3,18 @@ info in wiki.osdev.org */
 #ifndef __NUT_KERNEL__
 #define __NUT_KERNEL__
 
+void NutInit() { return };
+const char* NUT_VERSION = "v0.2.3";
+
 #include <nut/types.h>
 #include <nut/utils.h>
-
 #include <nut/colors.h>
 #include <nut/basics.h>
-
 #include <nut/stdio.h>
 
-
-const String NUT_VERSION = "v0.2.3";
-
 #include "functions.h"
-
-
 #include "log.h"
-
 #include "abort.h"
-
 #include "files.h"
 #include "network.h"
 //modules
