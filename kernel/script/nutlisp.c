3d2778d9f421fa5986022e90146d465215772a66
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-21 21:27:45 +0000

    Create nutlisp, nut kernel lisp dialect

---
+++ kernel/script/nutlisp.c
@@ -0,0 +1,6 @@
+/* See more lisp interpreter development in
+leohetsch.com/create-a-minimal-lisp-like-interpreter-in-c */
+#ifndef __NUT_LISP_INTERPRETER__
+#define __NUT_LISP_INTERPRETER__
+
+#endif //nut lisp interpreter
