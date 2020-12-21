0534a8e0ec0918797c5ac70772189c2150ae4563
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-21 21:43:30 +0000

    Create nutlisp parser module

--- kernel/script/nutlisp.c
+++ kernel/script/nutlisp.c
@@ -3,6 +3,12 @@ leohetsch.com/create-a-minimal-lisp-like-interpreter-in-c */
 #ifndef __NUT_LISP_INTERPRETER__
 #define __NUT_LISP_INTERPRETER__
 
+#include <nut/io.h>
+#include <nut/alloc.h>
+#include <nut/types.h>
+
 #include "operators.h"
+#include "parse.h"
+
 
 #endif //nutlisp interpreter
