1567335d6ada87abebfd43c8bb5889972e3096a0
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-10-03 12:28:33 +0100

    Add types module to `nutvm` project folder

---
+++ nutvm/types.h
@@ -0,0 +1,16 @@
+#ifndef __NUTVM_TYPES_H__
+#define __NUTVM_TYPES_H__
+
+#include <nut/types.h>
+
+#include "settings.h"
+#include "registers.h"
+
+
+typedef u8 byte_t;
+
+typedef byte[MEMSIZE*1024]   memory_t;
+typedef struct __REGISTERS__ registers_t;
+
+
+#endif // !__NUTVM_TYPES_H__
